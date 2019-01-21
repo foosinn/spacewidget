74308027d48262092bf2c009b678448b68a477fc
Stefan Schwarz <stefan@f2o.io>
2019-01-21 23:20:48 +0100

    release 0.1.3

--- spacewidget/setup.py
+++ spacewidget/setup.py
@@ -2,10 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='spacewidget',
-    version='0.1.0',
+    version='0.1.3',
     author='foosinn',
     author_email='foosinn@f2o.io',
     url='https://github.com/foosinn/spacewidget',
+    license="AGPL 3.0",
     packages=setuptools.find_packages(),
     package_data={
         'spacewidget': ['index.html'],
