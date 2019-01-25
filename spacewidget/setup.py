76097eebc8cf696e5e4f2e45b18c4e7e0797a5c4
Stefan Schwarz <stefan@f2o.io>
2019-01-25 19:25:24 +0100

    release 0.1.6

--- spacewidget/setup.py
+++ spacewidget/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='spacewidget',
-    version='0.1.5',
+    version='0.1.6',
     author='foosinn',
     author_email='foosinn@f2o.io',
     url='https://github.com/foosinn/spacewidget',
